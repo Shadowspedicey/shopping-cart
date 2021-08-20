fb8f15a8bc892bff85be2058c4e8033891440ac1
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-21 00:02:51 +0200

    Added a sections navbar to Shop

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Shop from "./components/Shop";
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -11,6 +12,7 @@ function App() {
 				<Navbar/>
 				<Switch>
 					<Route exact path="/" component={Home}></Route>
+					<Route path="/shop" component={Shop}></Route>
 				</Switch>
 				<Footer/>
 			</Router>
