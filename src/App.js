183181ae0df2c1b85195291651b14d7559d2f932
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-20 22:23:05 +0200

    Created the navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { HashRouter as Router } from 'react-router-dom';
+import Navbar from "./components/Navbar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+			<Router>
+				<Navbar/>
+
+			</Router>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import Logo from "../images/Logo.png"
+
+const Navbar = () =>
+{
+	return(
+		<nav id="navbar">
+			<Link to="/" style={{height: "100%", display: "flex", alignItems: "center"}}><img src={Logo} alt="Shadows PC World Logo" className="logo-img"></img></Link>
+			<ul>
+				<li><Link to="/">Home</Link></li>
+				<li><Link to="/shop">Shop</Link></li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navbar;
