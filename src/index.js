49b4d622a2000c95cbbdb938f7c3eb49a66a794c
Ahmed Khaled <shadowspedicey@gmail.com>
2021-08-24 15:02:05 +0200

    nit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import state from "./state/index";
 import { Provider } from "react-redux";
 import App from "./App";
-
-import state from "./state/index";
 import "./index.css";
+
 
 ReactDOM.render(
 	<Provider store={state}>
